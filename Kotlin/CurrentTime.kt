fbb6a14770771bc574a6ba7092e7b5890210c9eb
BuildTools <unconfigured@null.spigotmc.org>
2019-10-18 19:55:43 -0400

    Added demo of formatting time in Kotlin.

--- Kotlin/CurrentTime.kt
+++ Kotlin/CurrentTime.kt
@@ -1,5 +1,13 @@
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 fun main() {
     val current = LocalDateTime.now()
     println("Current Date and Time is: $current")
+
+    val timeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss")
+    val timeFormatted = current.format(timeFormatter)
+
+
+    println("The formatted current date time is: $timeFormatted") 
 }
